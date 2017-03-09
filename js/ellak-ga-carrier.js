2af00d30dcd08ea3e6f6d68bb5f3f69a8d72132d
David Bromoiras <bromoiras.david@gmail.com>
2017-03-09 12:07:04 +0200

    first commit

---
+++ js/ellak-ga-carrier.js
@@ -0,0 +1,7 @@
+/* 
+ * This file is deliberately left empty, as it acts as a reference point for the
+ * appropriate analytics snippets to be appended to, using wp_add_inline_script.
+ * This sequence takes place in the main plugin file, ellak-ga-plugin.php.
+ */
+
+
